2fd32aba4151a5944381bd17aa1ed6a4ceb336d2
anguske <anguske@aaxischina.com>
2024-10-23 15:21:42 +0800

    encode_wallet

---
+++ contracts/level0-base.sol
@@ -0,0 +1,549 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Counter {
+    uint256 public count;
+
+    function get() public view returns (uint256) {
+        return count;
+    }
+
+    function inc() public {
+        count += 1;
+    }
+
+    function dec() public {
+        count -= 1;
+    }
+}
+
+
+contract Primitives {
+    bool public boo = true;
+
+  
+    uint8 public u8 = 1;
+    uint256 public u256 = 456;
+    uint256 public u = 123; // uint is an alias for uint256
+
+ 
+    int8 public i8 = -1;
+    int256 public i256 = 456;
+    int256 public i = -123; // int is same as int256
+
+    // minimum and maximum of int
+    int256 public minInt = type(int256).min;
+    int256 public maxInt = type(int256).max;
+
+    address public addr = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+
+    bytes1 a = 0xb5; //  [10110101]
+    bytes1 b = 0x56; //  [01010110]
+
+
+    bool public defaultBoo; // false
+    uint256 public defaultUint; // 0
+    int256 public defaultInt; // 0
+    address public defaultAddr; // 0x0000000000000000000000000000000000000000
+}
+
+
+
+contract Variables {
+    // State variables are stored on the blockchain.
+    string public text = "Hello";
+    uint256 public num = 123;
+
+    function doSomething() public view {
+        // Local variables are not saved to the blockchain.
+        uint256 i = 456;
+
+        // Here are some global variables
+        uint256 timestamp = block.timestamp; // Current block timestamp
+        address sender = msg.sender; // address of the caller
+    }
+}
+
+
+contract Constants {
+    address public constant MY_ADDRESS =
+        0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint256 public constant MY_UINT = 123;
+}
+
+
+contract Immutable {
+    // coding convention to uppercase constant variables
+    address public immutable MY_ADDRESS;
+    uint256 public immutable MY_UINT;
+
+    constructor(uint256 _myUint) {
+        MY_ADDRESS = msg.sender;
+        MY_UINT = _myUint;
+    }
+}
+
+
+contract SimpleStorage {
+    uint256 public num;
+
+    function set(uint256 _num) public {
+        num = _num;
+    }
+    function get() public view returns (uint256) {
+        return num;
+    }
+}
+
+
+contract EtherUnits {
+    uint256 public oneWei = 1 wei;
+    // 1 wei is equal to 1
+    bool public isOneWei = (oneWei == 1);
+
+    uint256 public oneGwei = 1 gwei;
+    // 1 gwei is equal to 10^9 gwei
+    bool public isOneGwei = (oneGwei == 1e9);
+
+    uint256 public oneEther = 1 ether;
+    // 1 ether is equal to 10^18 wei
+    bool public isOneEther = (oneEther == 1e18);
+}
+
+
+contract Gas {
+    uint256 public i = 0;
+    function forever() public {
+        while (true) {
+            i += 1;
+        }
+    }
+}
+
+
+contract IfElse {
+    function foo(uint256 x) public pure returns (uint256) {
+        if (x < 10) {
+            return 0;
+        } else if (x < 20) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    function ternary(uint256 _x) public pure returns (uint256) {
+        // if (_x < 10) {
+        //     return 1;
+        // }
+        // return 2;
+
+        // shorthand way to write if / else statement
+        // the "?" operator is called the ternary operator
+        return _x < 10 ? 1 : 2;
+    }
+}
+
+
+contract Loop {
+    function loop() public pure {
+        for (uint256 i = 0; i < 10; i++) {
+            if (i == 3) {
+                continue;
+            }
+            if (i == 5) {
+                break;
+            }
+        }
+        uint256 j;
+        while (j < 10) {
+            j++;
+        }
+    }
+}
+
+contract Mapping {
+    mapping(address => uint256) public myMap;
+
+    function get(address _addr) public view returns (uint256) {
+        return myMap[_addr];
+    }
+
+    function set(address _addr, uint256 _i) public {
+        myMap[_addr] = _i;
+    }
+
+    function remove(address _addr) public {
+        delete myMap[_addr];
+    }
+}
+
+contract NestedMapping {
+    mapping(address => mapping(uint256 => bool)) public nested;
+
+    function get(address _addr1, uint256 _i) public view returns (bool) {
+        return nested[_addr1][_i];
+    }
+
+    function set(address _addr1, uint256 _i, bool _boo) public {
+        nested[_addr1][_i] = _boo;
+    }
+
+    function remove(address _addr1, uint256 _i) public {
+        delete nested[_addr1][_i];
+    }
+}
+
+
+contract Array {
+    uint256[] public arr;
+    uint256[] public arr2 = [1, 2, 3];
+    uint256[10] public myFixedSizeArr;
+
+    function get(uint256 i) public view returns (uint256) {
+        return arr[i];
+    }
+
+    function getArr() public view returns (uint256[] memory) {
+        return arr;
+    }
+
+    function push(uint256 i) public {
+        arr.push(i);
+    }
+
+    function pop() public {
+        arr.pop();
+    }
+
+    function getLength() public view returns (uint256) {
+        return arr.length;
+    }
+
+    function remove(uint256 index) public {
+        delete arr[index];
+    }
+
+    function examples() external pure {
+        // create array in memory, only fixed size can be created
+        uint256[] memory a = new uint256[](5);
+    }
+}
+
+
+
+contract ArrayRemoveByShifting {
+    uint256[] public arr;
+
+    function remove(uint256 _index) public {
+        require(_index < arr.length, "index out of bound");
+        for (uint256 i = _index; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr.pop();
+    }
+
+    function test() external {
+        arr = [1, 2, 3, 4, 5];
+        remove(2);
+        assert(arr[0] == 1);
+        assert(arr[1] == 2);
+        assert(arr[2] == 4);
+        assert(arr[3] == 5);
+        assert(arr.length == 4);
+
+        arr = [1];
+        remove(0);
+        assert(arr.length == 0);
+    }
+}
+
+contract Enum {
+    enum Status {
+        Pending,
+        Shipped,
+        Accepted,
+        Rejected,
+        Canceled
+    }
+
+    Status public status;
+    // Returns uint
+    // Pending  - 0
+    // Shipped  - 1
+    // Accepted - 2
+    // Rejected - 3
+    // Canceled - 4
+    function get() public view returns (Status) {
+        return status;
+    }
+
+    function set(Status _status) public {
+        status = _status;
+    }
+
+    function cancel() public {
+        status = Status.Canceled;
+    }
+
+    function reset() public {
+        delete status;
+    }
+}
+
+contract Todos {
+    struct Todo {
+        string text;
+        bool completed;
+    }
+
+    Todo[] public todos;
+
+    function create(string calldata _text) public {
+        todos.push(Todo(_text, false));
+
+        todos.push(Todo({text: _text, completed: false}));
+
+        Todo memory todo;
+        todo.text = _text;
+        todos.push(todo);
+    }
+
+    function updateText(uint256 _index, string calldata _text) public {
+        Todo storage todo = todos[_index];
+        todo.text = _text;
+    }
+
+    function toggleCompleted(uint256 _index) public {
+        Todo storage todo = todos[_index];
+        todo.completed = !todo.completed;
+    }
+}
+
+
+contract DataLocations {
+    uint256[] public arr;
+    mapping(uint256 => address) map;
+
+    struct MyStruct {
+        uint256 foo;
+    }
+
+    mapping(uint256 => MyStruct) myStructs;
+
+    function f() public {
+        _f(arr, map, myStructs[1]);
+        MyStruct storage myStruct = myStructs[1];
+        MyStruct memory myMemStruct = MyStruct(0);
+    }
+
+    function _f(
+        uint256[] storage _arr,
+        mapping(uint256 => address) storage _map,
+        MyStruct storage _myStruct
+    ) internal {
+        arr = _arr;
+    }
+
+
+    // You can return memory variables
+    function g(uint256[] memory _arr) public returns (uint256[] memory) {
+        // do something with memory array
+    }
+
+    function h(uint256[] calldata _arr) external {
+        // do something with calldata array
+    }
+}
+
+
+interface ITest {
+    function val() external view returns (uint256);
+    function test() external;
+}
+
+contract Callback {
+    uint256 public val;
+
+    fallback() external {
+        val = ITest(msg.sender).val();
+    }
+
+    function test(address target) external {
+        ITest(target).test();
+    }
+}
+
+contract TestStorage {
+    uint256 public val;
+
+    function test() public {
+        val = 123;
+        bytes memory b = "";
+       (bool success, ) =   msg.sender.call(b);
+       require(success,'error');
+    }
+}
+
+contract Function {
+    // Functions can return multiple values.
+    function returnMany() public pure returns (uint256, bool, uint256) {
+        return (1, true, 2);
+    }
+
+    // Return values can be named.
+    function named() public pure returns (uint256 x, bool b, uint256 y) {
+        return (1, true, 2);
+    }
+
+    // Return values can be assigned to their name.
+    // In this case the return statement can be omitted.
+    function assigned() public pure returns (uint256 x, bool b, uint256 y) {
+        x = 1;
+        b = true;
+        y = 2;
+    }
+
+    // Use destructuring assignment when calling another
+    // function that returns multiple values.
+    function destructuringAssignments()
+        public
+        pure
+        returns (uint256, bool, uint256, uint256, uint256)
+    {
+        (uint256 i, bool b, uint256 j) = returnMany();
+
+        // Values can be left out.
+        (uint256 x,, uint256 y) = (4, 5, 6);
+
+        return (i, b, j, x, y);
+    }
+
+    // Cannot use map for either input or output
+
+    // Can use array for input
+    function arrayInput(uint256[] memory _arr) public {}
+
+    // Can use array for output
+    uint256[] public arr;
+
+    function arrayOutput() public view returns (uint256[] memory) {
+        return arr;
+    }
+}
+
+// Call function with key-value inputs
+contract XYZ {
+    function someFuncWithManyInputs(
+        uint256 x,
+        uint256 y,
+        uint256 z,
+        address a,
+        bool b,
+        string memory c
+    ) public pure returns (uint256) {}
+
+    function callFunc() external pure returns (uint256) {
+        return someFuncWithManyInputs(1, 2, 3, address(0), true, "c");
+    }
+
+    function callFuncWithKeyValue() external pure returns (uint256) {
+        return someFuncWithManyInputs({
+            a: address(0),
+            b: true,
+            c: "c",
+            x: 1,
+            y: 2,
+            z: 3
+        });
+    }
+}
+
+
+contract Error {
+    function testRequire(uint256 _i) public pure {
+        require(_i > 10, "Input must be greater than 10");
+    }
+
+    function testRevert(uint256 _i) public pure {
+        if (_i <= 10) {
+            revert("Input must be greater than 10");
+        }
+    }
+
+    uint256 public num;
+
+    function testAssert() public view {
+        assert(num == 0);
+    }
+
+    // custom error
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function testCustomError(uint256 _withdrawAmount) public view {
+        uint256 bal = address(this).balance;
+        if (bal < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: bal,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+    }
+}
+
+
+contract FunctionModifier {
+    address public owner;
+    uint256 public x = 10;
+    bool public locked;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    // Modifiers can take inputs. This modifier checks that the
+    // address passed in is not the zero address.
+    modifier validAddress(address _addr) {
+        require(_addr != address(0), "Not valid address");
+        _;
+    }
+
+    function changeOwner(address _newOwner)
+        public
+        onlyOwner
+        validAddress(_newOwner)
+    {
+        owner = _newOwner;
+    }
+
+    modifier noReentrancy() {
+        require(!locked, "No reentrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    function decrement(uint256 i) public noReentrancy {
+        x -= i;
+
+        if (i > 1) {
+            decrement(i - 1);
+        }
+    }
+}
+
+contract Event {
+    event Log(address indexed sender, string message);
+    event AnotherLog();
+
+    function test() public {
+        emit Log(msg.sender, "Hello World!");
+        emit Log(msg.sender, "Hello EVM!");
+        emit AnotherLog();
+    }
+}
